d5c1f093a1b8bf33537f50189b9a50a20351fd45
g.salvio <giuseppe.salvio@finconsgroup.com>
2021-04-01 14:44:39 +0200

    add http search and post

--- src/app/pagine-esempio/http-call-ex/http-call-ex.component.ts
+++ src/app/pagine-esempio/http-call-ex/http-call-ex.component.ts
@@ -11,10 +11,11 @@ import { People } from "../../domain/people";
 export class HttpCallExComponent implements OnInit {
   peoples: People[];
   peoplesSearchById: People;
-  peoplePost: People = {
+  peoplePut: People = {
     id: null,
     name: "",
   };
+  name: string;
   value1: number;
   constructor(private peopleService: PeopleService) {}
 
@@ -52,17 +53,18 @@ export class HttpCallExComponent implements OnInit {
     );
   }
 
-  postPeople(peoplePost: People): void {
-    this.peopleService.postPeople(peoplePost).subscribe(
+  postPeople(name: string): void {
+    const temp: People = { name: name };
+    this.peopleService.postPeople(temp).subscribe(
       (val) => {
-        console.log("PUT call successful value returned in body", val);
-        this.peoplesSearchById = val;
+        console.log("POST call successful value returned in body", val);
       },
       (response) => {
-        console.log("PUT call in error", response);
+        console.log("POST call in error", response);
       },
       () => {
-        console.log("The PUT observable is now completed.");
+        this.getPeople();
+        console.log("The POST observable is now completed.");
       }
     );
   }
